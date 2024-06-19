b9eb47a715d87dd09b4b0d91a664142750b57067
Julia E. Mette <juliaemanuele.mette@philips.com>
2024-06-19 11:59:35 -0300

    arrumando integração

--- Main.java
+++ Main.java
@@ -6,9 +6,7 @@ public class Main {
   static Scanner scanner = new Scanner(System.in);
 
   public static void main(String[] args) {
-
     menu();
-
   }
 
   private static void menu() {
@@ -23,42 +21,54 @@ public class Main {
     valor = entradaMontante();
     extenso = extenso(valor);
     drawComplete(nome, extenso, data, valor);
-    // data = date();
-    // drawComplete(nome, extenso, data, valor);
+    data = date();
+    drawComplete(nome, extenso, data, valor);
   }
 
   private static void draw() {
+
     System.out.println("|:---------------------------------------------------------------------------------------:|");
     System.out.println("| Cheque n0182 | Comp | Banco | Agencia | C1 |  Conta  |   | Cheque N | C3 | R$           |");
     System.out.println("|              | 018  |  777  |   0444  | 02 | 6780913 | A |   0182   | 00 |              |");
     System.out.println("|              | Pago por este                                                            |");
     System.out.println("|              | cheque a quantia de                                                      |");
-    System.out.println("|              | a                           ou a sua ordem                               |");
-    System.out.println("|              | BANCO                  Blumenau,                                         |");
+    System.out.println("|              | a                                                         ou a sua ordem |");
+    System.out.println("|              | BANCO                          Blumenau,                                 |");
     System.out.println("|              | AGENCIA                                                                  |");
-    System.out.println("|              |                                                       ASSINATURA         |");
+    System.out.println("|              |                                            ASSINATURA                    |");
     System.out.println("|:---------------------------------------------------------------------------------------:|");
-
   }
 
   private static void drawComplete(String nome, String extenso, String data, float valor) {
-    System.out.println("|:-----------------------------------------------------------------------------------------:|");
-    System.out.println("| Cheque n0182 | Comp | Banco | Agência | C1 |  Conta  |   | Cheque N | C3 | R$ " + valor);
-    System.out.println("|              | 018  |  777  |   0444  | 02 | 6780913 | A |   0182   | 00 |              |");
-    System.out.println("|              | Pago por este                                                            |");
-    System.out.println("|              | cheque a quantia de " + extenso);
-    System.out.println("|              | a " + nome + "              ou a sua ordem                               |");
-    System.out.println("|              | BANCO                  Blumenau, " + data);
-    System.out.println("|              | AGENCIA                                                                  |");
-    System.out.println("|              |                                                       ASSINATURA         |");
-    System.out.println("|:---------------------------------------------------------------------------------------:|");
 
+    String linha1 = "|:---------------------------------------------------------------------------------------:|";
+    String linha2 = String
+        .format("| Cheque n0182 | Comp | Banco | Agência | C1 |  Conta  |   | Cheque N | C3 | R$ %-9.2f |", valor);
+    String linha3 = "|              | 018  |  777  |   0444  | 02 | 6780913 | A |   0182   | 00 |              |";
+    String linha4 = "|              | Pago por este                                                            |";
+    String linha5 = String.format("|              | cheque a quantia de %-52s |", extenso);
+    String linha6 = String.format("|              | a %-55s ou à sua ordem |", nome);
+    String linha7 = String.format("|              | BANCO                          Blumenau, %-32s|", data);
+    String linha8 = "|              | AGÊNCIA                                                                  |";
+    String linha9 = "|              |                                            ASSINATURA                    |";
+    String linha10 = "|:---------------------------------------------------------------------------------------:|";
+
+    System.out.println(linha1);
+    System.out.println(linha2);
+    System.out.println(linha3);
+    System.out.println(linha4);
+    System.out.println(linha5);
+    System.out.println(linha6);
+    System.out.println(linha7);
+    System.out.println(linha8);
+    System.out.println(linha9);
+    System.out.println(linha10);
   }
 
   private static float entradaMontante() {
 
     while (true) {
-      System.out.print("Informe um valor para o cheque que seja entre R$00.01 a R$99.99: ");
+      System.out.println("Informe um valor para o cheque que seja entre R$00.01 a R$99.99: ");
       float floatInput = scanner.nextFloat();
 
       if (floatInput > 0.00 && floatInput < 100.00) {
@@ -69,15 +79,24 @@ public class Main {
   }
 
   private static String extenso(float input) {
-
+    int i = 0;
     String inputNumber = String.format("%.2f", input);
     inputNumber = inputNumber.replace(",", "");
     String finalString = "";
 
-    int[] array = new int[inputNumber.length()];
+    int[] array;
 
-    for (int i = 0; i < inputNumber.length(); i++) {
-      array[i] = Character.getNumericValue(inputNumber.charAt(i));
+    if (inputNumber.length() == 3) {
+      array = new int[4];
+      array[0] = 0;
+      for (i = 1; i < 4; i++) {
+        array[i] = Character.getNumericValue(inputNumber.charAt(i - 1));
+      }
+    } else {
+      array = new int[inputNumber.length()];
+      for (i = 0; i < inputNumber.length(); i++) {
+        array[i] = Character.getNumericValue(inputNumber.charAt(i));
+      }
     }
 
     if (array[0] == 1) {
@@ -165,11 +184,7 @@ public class Main {
       }
     }
 
-    if (array[0] != 1 && array[0] != 0 && array[1] != 0) {
-      finalString += "e ";
-    }
-
-    if (array[0] != 1) {
+    if (array[0] == 0 && array[1] != 0) {
       switch (array[1]) {
         case 1:
           finalString += "um ";
@@ -212,98 +227,104 @@ public class Main {
       }
     }
 
+    if (array[0] != 1 && array[0] != 0 && array[1] != 0) {
+      finalString += "e ";
+    }
+
     if (array[0] == 0 && array[1] == 1) {
       finalString += "real ";
     } else if (array[0] != 0) {
       finalString += "reais ";
+    } else if (array[0] == 0 && array[1] != 0) {
+      finalString += "reais ";
     }
 
-    if (array[2] != 0) {
+    if (array[0] != 0 && array[1] != 0 && array[2] != 0) {
       finalString += "e ";
+    }
 
-      if (array[2] == 1) {
-        switch (array[3]) {
-          case 0:
-            finalString += "dez ";
-            break;
+    if (array[2] == 1) {
+      switch (array[3]) {
+        case 0:
+          finalString += "dez ";
+          break;
 
-          case 1:
-            finalString += "onze ";
-            break;
+        case 1:
+          finalString += "onze ";
+          break;
 
-          case 2:
-            finalString += "doze ";
-            break;
+        case 2:
+          finalString += "doze ";
+          break;
 
-          case 3:
-            finalString += "treze ";
-            break;
+        case 3:
+          finalString += "treze ";
+          break;
 
-          case 4:
-            finalString += "quatorze ";
-            break;
+        case 4:
+          finalString += "quatorze ";
+          break;
 
-          case 5:
-            finalString += "quinze ";
-            break;
+        case 5:
+          finalString += "quinze ";
+          break;
 
-          case 6:
-            finalString += "dezesseis ";
-            break;
+        case 6:
+          finalString += "dezesseis ";
+          break;
 
-          case 7:
-            finalString += "dezessete ";
-            break;
+        case 7:
+          finalString += "dezessete ";
+          break;
 
-          case 8:
-            finalString += "dezoito ";
-            break;
+        case 8:
+          finalString += "dezoito ";
+          break;
 
-          case 9:
-            finalString += "dezenove ";
-            break;
+        case 9:
+          finalString += "dezenove ";
+          break;
 
-          default:
-            break;
-        }
+        default:
+          break;
+      }
 
-      } else {
-        switch (array[2]) {
-          case 2:
-            finalString += "vinte ";
-            break;
+    } else {
+      switch (array[2]) {
+        case 2:
+          finalString += "vinte ";
+          break;
 
-          case 3:
-            finalString += "trinta ";
-            break;
+        case 3:
+          finalString += "trinta ";
+          break;
 
-          case 4:
-            finalString += "quarenta ";
-            break;
+        case 4:
+          finalString += "quarenta ";
+          break;
 
-          case 5:
-            finalString += "cinquenta ";
-            break;
+        case 5:
+          finalString += "cinquenta ";
+          break;
 
-          case 6:
-            finalString += "sessenta ";
-            break;
+        case 6:
+          finalString += "sessenta ";
+          break;
 
-          case 7:
-            finalString += "setenta ";
-            break;
+        case 7:
+          finalString += "setenta ";
+          break;
 
-          case 8:
-            finalString += "oitenta ";
-            break;
+        case 8:
+          finalString += "oitenta ";
+          break;
 
-          case 9:
-            finalString += "noventa ";
-            break;
+        case 9:
+          finalString += "noventa ";
+          break;
 
-          default:
-            break;
-        }
+        default:
+          break;
       }
     }
 
@@ -356,7 +377,7 @@ public class Main {
 
     if (array[2] == 0 && array[3] == 1) {
       finalString += "centavo ";
-    } else if (array[3] >= 2 || array[2] != 0 && array[3] == 0) {
+    } else if (array[3] >= 2 || array[2] != 0) {
       finalString += "centavos ";
     }
 
@@ -376,7 +397,96 @@ public class Main {
     return nome;
   }
 
-  private static void date() {
+  private static String date() {
+    Scanner scannerDate = new Scanner(System.in);
+    String date = "";
+
+    while (date == "") {
+      System.out.println("Informe a data: Ex: 00/00/0000 ");
+      String inputDate = scannerDate.nextLine();
+
+      if (!inputDate.matches("\\d{2}/\\d{2}/\\d{4}")) {
+        System.out.println("Entrada invalida, digite a data no formato indicado");
+      } else {
+        String[] splitedInputdate = inputDate.trim().split("/");
+
+        int day = Integer.parseInt(splitedInputdate[0]);
+        int month = Integer.parseInt(splitedInputdate[1]);
+        int year = Integer.parseInt(splitedInputdate[2]);
+
+        if (year < 2000 || year > 2030) {
+          System.out.println("Ano informado fora do periodo permitido! (De 2000 a 2030)");
+        } else if (month < 1 || month > 12) {
+          System.out.println("Mes informado não existe, favor informe um mes entre 1 e 12");
+        } else {
+          if (month == 2) {
+            if (day < 1 || day > 28) {
+              System.out.println("Dia informado nao existe para o mes");
+            } else {
+              date = String.valueOf(day) + " de Fevereiro de " + String.valueOf(year);
+            }
+          }
+          if (month == 4 || month == 6 || month == 9 || month == 11) {
+            if (day < 1 || day > 30) {
+              System.out.println("Dia informado nao existe para o mes");
+            } else {
+              switch (month) {
+                case 4:
+                  date = String.valueOf(day) + " de Abril de " + String.valueOf(year);
+                  break;
+                case 6:
+                  date = String.valueOf(day) + " de Junho de " + String.valueOf(year);
+                  break;
+                case 9:
+                  date = String.valueOf(day) + " de Setembro de " + String.valueOf(year);
+                  break;
+                case 11:
+                  date = String.valueOf(day) + " de Novembro de " + String.valueOf(year);
+                  break;
+
+                default:
+                  break;
+              }
+
+            }
+          }
+          if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10
+              || month == 12) {
+            if (day < 1 || day > 31) {
+              System.out.println("Dia informado nao existe para o mes");
+            } else {
+              switch (month) {
+                case 1:
+                  date = String.valueOf(day) + " de Janeiro de " + String.valueOf(year);
+                  break;
+                case 3:
+                  date = String.valueOf(day) + " de Marco de " + String.valueOf(year);
+                  break;
+                case 5:
+                  date = String.valueOf(day) + " de Maio de " + String.valueOf(year);
+                  break;
+                case 7:
+                  date = String.valueOf(day) + " de Julho de " + String.valueOf(year);
+                  break;
+                case 8:
+                  date = String.valueOf(day) + " de Agosto de " + String.valueOf(year);
+                  break;
+                case 10:
+                  date = String.valueOf(day) + " de Outubro de " + String.valueOf(year);
+                  break;
+                case 12:
+                  date = String.valueOf(day) + " de Dezembro de " + String.valueOf(year);
+                  break;
+                default:
+                  break;
+              }
+            }
+          }
+        }
+      }
+
+    }
+    return date;
   }
 
 }
